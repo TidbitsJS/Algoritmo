738cb6346d9f884c06ac4a1316e77ef4451022d0
Tidbits JS <tidbitsjs@gmail.com>
2021-02-28 22:30:42 +0530

    Hash Table

---
+++ Hash Table/Hash.js
@@ -0,0 +1,68 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) + i) % this.data.length;
+    }
+
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+    console.log(this.data);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket.length) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return {
+            data: [currentBucket[i][0], currentBucket[i][1]],
+            location: address,
+          };
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) keysArray.push(this.data[i][0][0]);
+    }
+
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(100);
+
+myHashTable.set("grapes", 1000);
+let found = myHashTable.get("grapes");
+console.log("Found", found);
+
+myHashTable.set("apple", 100);
+found = myHashTable.get("apple");
+console.log("Found", found);
+
+myHashTable.set("oranges", 50);
+found = myHashTable.get("oranges");
+console.log("Found", found);
+
+let keys = myHashTable.keys();
+console.log(keys);
---
+++ Hash Table/readme.md
@@ -0,0 +1,7 @@
+### Hash in JS
+
+Implementing a simple Hash Table in JavaScript, where, you get to -
+
+1. Insert item (set)
+2. Find item (get)
+3. Display all items (keys)
