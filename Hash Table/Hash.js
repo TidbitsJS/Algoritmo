e22f1dcfe0fa9f1d5610a0bd98d0ac57905b3fd1
Tidbits JS <tidbitsjs@gmail.com>
2021-03-04 00:41:38 +0530

    delete item validations

--- Hash Table/Hash.js
+++ Hash Table/Hash.js
@@ -1,31 +1,40 @@
 class HashTable {
   constructor(size) {
+    // Set Hash Table size
     this.data = new Array(size);
   }
 
+  // Generate a location using simple hash logic
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
+      // Location mapping
       hash = (hash + key.charCodeAt(i) + i) % this.data.length;
     }
 
     return hash;
   }
 
+  // Set the location of item in hash
   set(key, value) {
     let address = this._hash(key);
     if (!this.data[address]) {
       this.data[address] = [];
     }
 
+    // Insert item at the desired location with [key, value] pair
     this.data[address].push([key, value]);
     console.log(this.data);
     return this.data;
   }
 
+  // Get the location of item in hash table
   get(key) {
     let address = this._hash(key);
     const currentBucket = this.data[address];
+    // Item at the location is not found , return undefined
+    if (!currentBucket) return undefined;
+
     if (currentBucket.length) {
       for (let i = 0; i < currentBucket.length; i++) {
         if (currentBucket[i][0] === key) {
@@ -39,12 +48,21 @@ class HashTable {
     return undefined;
   }
 
+  // Delete item
   deleteItem(value) {
     let item = this.get(value);
+    console.log("item", item);
+
+    // Item is not present in hash table
+    if (item === undefined)
+      return console.log("Can't delete, no such item found");
+
+    // Delete item present at that location from hash table
     delete this.data[item.location];
     return console.log("Deleted", item.data[0]);
   }
 
+  // Display all items
   keys() {
     const keysArray = [];
     for (let i = 0; i < this.data.length; i++) {
@@ -75,3 +93,6 @@ console.log(keys);
 myHashTable.deleteItem("apple");
 keys = myHashTable.keys();
 console.log(keys);
+
+myHashTable.deleteItem("fruits");
+console.log(myHashTable);
--- Hash Table/readme.md
+++ Hash Table/readme.md
@@ -5,4 +5,4 @@ Implementing a simple Hash Table in JavaScript, where, you get to -
 1. Insert item (set)
 2. Find item (get)
 3. Display all items (keys)
-4. Delete item (deletItem)
+4. Delete item (deleteItem)
